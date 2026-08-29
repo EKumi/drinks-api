ea8ab6905a3a085e8cfcba39dbc1950b1ef94638
EKumi <www.estee7000@gmail.com>
2026-08-29 14:59:19 +0000

    Import existing Drinks API EC2 into Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,12 @@ terraform {
 provider "aws" {
   region = "eu-north-1"
 }
+
+resource "aws_instance" "drinks_api" {
+  ami           = "ami-07b8fb6bd3e9627a6"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "drinks-api-server"
+  }
+}
